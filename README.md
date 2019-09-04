ba232fe302072f76a160db5c9f619e20813ae892
wgaul <wgaul@hotmail.com>
2019-09-04 17:43:45 +0100

    Added non-detection data to p/a column.  Create a presence-only data frame.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ contact: Willson Gaul wgaul@hotmail.com
 
 This script is for working with data of weekly bird observations on the campus of University College Dublin from 1973 to 1998.  Observations were recorded as either presence/absence or using abundance codes.  The records are the combined observations of four staff members in UCD.  
 
-This script uses the data available on Zenodo at: https://zenodo.org/record/3250828#.XQuWxyZ7loB
+This script uses the data available on Zenodo at: https://doi.org/10.5281/zenodo.3250827
 
 
 
--- merge_ledger_data.R
+++ merge_ledger_data.R
@@ -1,18 +1,29 @@
 #######################
 ## Merge Casey's spreadsheets of annual ledger data into a single .csv file
 ##
+## inputs: the .csv files of digitized bird data (one file for each year)
+##        available from https://doi.org/10.5281/zenodo.3250827
+##
+## outputs:   * bird - data frame with a row for every species x week grid cell
+##                    in the original paper data logs.  This includes a presence/
+##                    absence column that assumes empty grid cells in the 
+##                    original paper log are non-detection data.
+##            * bird_presence_only - data frame with a row for every bird x 
+##                    week detection record.
+##
 ## author: Willson Gaul wgaul@hotmail.com
 ## created: 16 April 2019
-## last modified: 19 June 2019
+## last modified: 4 Sep 2019
 ########################
 
+# user will need to set working directory and path to data
+warning("User must set working directory and path to data")
 setwd("~/Documents/Data_Analysis/UCD/ucd_birds/")
+# user will need to set the path to read the data
+fnames <- list.files("~/Documents/UCD/birds/casey_project/data/casey_digitised_ledgers/")
 library(Hmisc)
 library(tidyverse)
 library(lubridate)
-
-# user will need to set the path to read the data
-fnames <- list.files("~/Documents/UCD/birds/casey_project/data/casey_digitised_ledgers/")
 
 fnames <- fnames[grepl(pattern = ".*csv$", x = fnames)]
 
@@ -36,9 +47,9 @@ bird <- lapply(bird, FUN = function(x) {
 # fix 1982 week end date issues
 # a typo in CP's original file
 colnames(bird$ledger_1982)[colnames(bird$ledger_1982) == "24/06"] <- "25/06"
-# a mistake by Kevin where he used a Thursday as the week end date instead of
-# using Friday as he had been.  This matters b/c 1 Jan was on Friday in 1982, 
-# and later I calculate week number as number of 7 day periods since 1 Jan.
+# a mistake in original ledgers where Thursday was used as the week end date 
+# instead of using Friday as before.  This matters b/c 1 Jan was on Friday in 
+# 1982 and later I calculate week number as number of 7 day periods since 1 Jan.
 # So to adequately represent successive weeks of survey, I need to make the
 # week ending days all be on Friday (or later, Sat and Sun are ok).
 colnames(bird$ledger_1982)[colnames(bird$ledger_1982) == "01/07"] <- "02/07"
@@ -69,11 +80,17 @@ bird$week[bird$week %nin% as.character(1:54)] <- week(
           format = "%d/%m/%Y"))
 
 # make a presence-absence column
+# This treats empty cells in the original paper log sheets as non-detection data
 pa <- function(x){
-  x[x > 0] <- 1
+  x[is.na(x) | x == "?"] <- 0 # treat questions marks and empty cells as absences
+  x[x != 0] <- 1 # treat any other entry as a presence
   x}
-bird$observed_pa <- pa(bird$observation)
+bird$observed <- pa(bird$observation)
 
 # spread if you want
 # bird_wide <- spread(bird[, colnames(bird) != "Notes"], key = week, 
 #                     value = observation)
+
+# subset to only occurrence records
+bird_presence_only <- bird[bird$observed == 1, ]
+colnames(bird_presence_only)[1] <- "Species"
